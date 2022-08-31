66b09d57d0fc8a8dae82d0b6223920b92c29a96d
Christopher Marfia <chris.marfia@daugherty.com>
2022-08-31 17:13:42 -0500

    Added comments on how to implement Requirement #3.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,40 @@ import PokedexList from './PokedexList';
 function App() {
   const [selectedPokedex, setSelectedPokedex] = useState(null);
 
+  /***
+   * TODO #1 - create a helper function that will take in a pokedex
+   * name as a string and update the selectedPokedex hook with
+   * the new value.
+   * 
+   * Named: selectPokedex
+   * 
+   * Please look at this example from typerace on how to do that!
+   * 
+   * function chooseSnippet(selectedSnippet) {
+   *   setSnippet(selectedSnippet);
+   *   setGameState({
+   *     ...initialGameState,
+   *     startTime: new Date().getTime(),
+   *   })
+   * }
+   */
+
   if (selectedPokedex === null) {
+    /***
+     * TODO #2 - We need to pass the helper function to this
+     * new component as a prop. 
+     * 
+     * Please look at this example from typerace on how to do that!
+     * pst. chooseSnippet
+     * 
+     * <SnippetSelector films={films} chooseSnippet={chooseSnippet} />
+     */
     return <PokedexList />;
   }
 
   return (
     <div className="App">
-      Have not implemented this yet!
+      This text will render when you have successfully implemented all the 5 different TODOs.
     </div>
   );
 }
--- src/PokedexList.jsx
+++ src/PokedexList.jsx
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Pokedex } from 'pokeapi-js-wrapper';
 
+/***
+ * TODO #3 - update the function signature to have props
+ * 
+ * Please look at this example from typerace on how to do that!
+ * 
+ * function SnippetSelector(props) { ... }
+ */
 function PokedexList() {
+    /***
+     * TODO #4 - pull out of the new prop selectPokedex from the param
+     * 
+     * Please look at this example from typerace on how to do that!
+     * 
+     * const { films, chooseSnippet } = props;
+     */
     const [hasError, setErrors] = useState(null);
     const [pokedexes, setPokedexes] = useState([]);
 
@@ -26,6 +40,14 @@ function PokedexList() {
     function renderPokdexes() {
         return pokedexes.map((pokedexData) => {
             const { name } = pokedexData;
+            /***
+             * TODO #5 - update the code below to call the new prop with the name
+             * of the pokedex when the button is clicked. 
+             * 
+             * Please look at this example from typerace on how to do that!
+             * 
+             * <button key={id} onClick={() => onSelection(title)}>{title}</button>
+             */
             return <li>{name} <button>View</button></li>
         })
     }
